7fafb636c89357f27fbe77b5bd9fa602564cc7eb
efsystems011-tech <efsystems011@gmail.com>
2026-08-22 14:30:47 -0400

    painel alunos

--- script.js
+++ script.js
@@ -60,11 +60,11 @@ const alunosIniciais = [
         documentos: [
             {
                 nome: "Relatório Pedagógico.pdf",
-                tamanho:"1.2MB",
+                tamanho: "1.2MB",
                 data: "2026-04-21"
             },
 
-             {
+            {
                 nome: "Plano de Atendimento.docx",
                 tamanho: "980 KB",
                 data: "05/08/2026"
@@ -91,7 +91,7 @@ const alunosIniciais = [
 
 let alunos = JSON.parse(
     localStorage.getItem("orientacaoAlunos")
-)||alunosIniciais;
+) || alunosIniciais;
 
 function salvarDados() {
     localStorage.setItem("orientacaoAlunos", JSON.stringify(alunos));
@@ -181,10 +181,10 @@ function renderizarAlunos(
 
     const filtrados = alunos.filter(
         aluno => aluno.nome
-        .toLowercase()
-        .includes(
-            filtro.toLowerCase()
-        )
+            .toLowercase()
+            .includes(
+                filtro.toLowerCase()
+            )
     );
 
 
@@ -196,7 +196,7 @@ function renderizarAlunos(
 
             div.className = "student";
 
-            if(
+            if (
                 alunoSelecionado && alunoSelecionado.id === aluno.id
             ) {
                 div.classList.add(
@@ -225,8 +225,557 @@ function renderizarAlunos(
 
 /*
 ===============================================
-    LISTA DE aLUNOS
+    SELECIONAR ALUNO
 
 ===============================================
 
 */
+
+function selecionarAluno(id) {
+    alunoSelecionado = alunos.find(
+        aluno = aluno.id === id
+    );
+
+    if (!alunoSelecionado)
+        return
+
+    document.getElementById(
+        "emptyState"
+    ).style.display = "none";
+
+    document.getElementById(
+        "studentProfile"
+    ).style.display = "block";
+
+
+    atualizarPerfil();
+
+    renderizarAlunos(
+        document.getElementById(
+            "studentSearch"
+        ).value
+    );
+
+    lucide.createIcons();
+}
+
+/*
+===============================================
+  PERFIL
+
+===============================================
+
+*/
+
+function atualizarPerfil() {
+    const aluno = alunoSelecionado;
+
+    document.getElementById(
+        "studentName"
+    ).textContent = aluno.nome;
+
+    document.getElementById(
+        "studentInfo"
+    ).textContent = `
+        ${aluno.turma} = ${aluno.idade} anos • matricula: ${aluno.matricula}
+    `;
+
+    document.getElementById(
+        "studentPhone"
+    ).textContent = aluno.telefone || "Não informado";
+
+    document.getElementById(
+        "studentEmail"
+    ).textContent.email || "Não informado";
+
+    document.getElementById(
+        "studentAvatar"
+    ).textContent = iniciais(aluno.nome);
+
+    document.getElementById(
+        "frequency"
+    ).textContent = `${aluno.frequencia}%`;
+
+    document.getElementById(
+        "performace"
+    ).textContent = aluno.desempenho;
+
+    renderizarAlunos();
+
+    renderizarDocumentos();
+
+    renderizarAudios();
+
+    renderizarResponsaveis();
+
+    lucide.createIcons();
+}
+
+// ========================================================
+// INICIAIS
+// ========================================================
+
+function iniciais(nome) {
+    return nome
+        .split(" ")
+        .slice(0, 2)
+        .map(
+            palavra =>
+                palavra[0]
+        )
+        .join("")
+        .toUpperCase();
+};
+
+// ========================================================
+// OCORRÊNCIAS
+// ========================================================
+
+function renderizarOcorrencias() {
+    const ocorrencias = alunoSelecionado.ocorrencias;
+
+    const recentes = ocorrencias.slice(-3).reverse();
+
+
+    const container = document.getElementById(
+        "recentOccurrences"
+    );
+
+    container.innerHTML = recentes.length
+        ? recentes.map(
+            criarOcorrenciaHTML
+        ).join("") : `
+                                <div class="empty-small">
+                                    Nenhuma ocorrência registrada.
+                                </div>
+                            `;
+
+    const todas = document.getElementById(
+        "allOccurrences"
+    );
+
+    if (todas) {
+        todas.innerHTML =
+            ocorrencias.length
+                ? ocorrencias
+                    .slice()
+                    .reverse()
+                    .map(
+                        criarOcorrenciaHTML
+                    )
+                    .join("")
+                : `
+                    <div class="empty-small>
+                        Nenhuma ocorrência registrada.
+                    </div>
+                `;
+    }
+}
+
+function criarOcorrenciaHTML(ocorrencia) {
+    return `
+    <div class="occurrence">
+        <div class="occurrence-icon">
+            <i data-lucide="${iconeOcorrencia(
+        ocorrencia.tipo
+    )}"></i>
+        </div>
+
+        <div>
+            <h4>
+                ${ocorrencia.tipo}
+            </h4>
+
+            <p>
+                ${ocorrencia.descrição}
+            </p>
+
+            <small>
+                ${formatarData(
+        ocorrencia.data
+    )}
+            </small>
+        </div>
+        </div>
+    `;
+}
+
+
+function iconeOcorrencia(tipo) {
+    const icones = {
+        Saúde: "heart-pulse",
+        Comportamento: "message-circle",
+        Aprendizagem: "book-open",
+        Frequência: "calendar-x",
+        Família: "users",
+        Outro: "clipboard"
+    };
+
+    return (
+        icones[tipo] || "clipboard"
+    );
+
+}
+
+// ========================================================
+// MODAL OCORRÊNCIA
+// ========================================================
+
+function abrirOcorrencia() {
+    if (!alunoSelecionado) {
+        alert("Selecione um aluno primeiro");
+        return;
+    };
+
+    document.getElementById(
+        "occurrenceDate"
+    ).value = new Date()
+        .toUTCString()
+        .split("T")[0];
+
+    document.getElementById(
+        "occurrenceModal"
+    ).classList.add("show");
+}
+
+document.getElementById(
+    "occurrenceForm"
+).addEventListener(
+    "submit",
+    function (event) {
+        event.preventDefault();
+
+        const novaOcorrencia = {
+            id: Date.now(),
+            tipo:
+                document.getElementById(
+                    "occurrenceType"
+                ).value,
+
+            data:
+                document.getElementById(
+                    "occurrenceDate"
+                ).value,
+
+            descrição:
+                document.getElementById(
+                    "occurrenceDescription"
+                ).value
+        };
+
+        alunoSelecionado.ocorrencia.push(
+            novaOcorrencia
+        );
+
+        salvarDados();
+
+        atualizarDashboard();
+
+        atualizarPerfil();
+
+        fecharModal(
+            "occurrenceModal"
+        );
+
+        this.reset();
+    }
+);
+
+// ========================================================
+// DOCUMENTOS
+// ========================================================
+
+
+function renderizarDocumentos() {
+
+    const documentos = alunoSelecionado.documentos;
+
+    const html = documentos.length
+        ? documentos.map(
+            criarDocumentoHTML
+        ).join("")
+        : `
+                        <div class="empty-small">
+                            Nenhum documentos anexado
+                        </div>
+                    `;
+
+    document.getElementById(
+        "documentList"
+    ).innerHTML =
+        documentos
+            .slice(0, 3)
+            .map(criarDocumentoHTML)
+            .join("");
+
+    document.getElementById(
+        "allDocuments"
+    ).innerHTML = html;
+
+    lucide.createIcons();
+}
+
+function criarDocumentoHTML(documento) {
+    return `
+        <div class="document">
+            <div class="document-icon">
+                <i data-lucide="file-text"></i>
+            </div>
+
+            <div>
+                <strong title="${documento.nome}">
+                    ${documento.nome}
+                </strong>
+                <small>
+                    ${documento.tamanho}
+                    •
+                    ${documento.data}
+                </small>
+            </div>
+        </div>
+    `;
+}
+
+// ========================================================
+// UPLOAD DE DOCUMENTO
+// ========================================================
+
+document.getElementById(
+    "documentUpload"
+).addEventListener(
+    "change",
+    function () {
+        const arquivo =
+            this.files[0];
+
+        if (!arquivo)
+            return;
+
+        alunoSelecionado.documentos.push({
+            nome: arquivo.name,
+            tamanho: formatarTamanho(
+                arquivo.size
+            ),
+            data: new Date()
+                .toLocaleDateString(
+                    "pt-BR"
+                )
+        });
+
+        salvarDados();
+
+        atualizarDashboard();
+
+        renderizarDocumentos();
+
+        this.value = "";
+    }
+);
+
+// ========================================================
+// ÁUDIOS
+// ========================================================
+
+
+function renderizarAudios() {
+
+    const audios = alunoSelecionado.audios;
+
+    const html = audios.length
+        ? audios.map(
+            criarAudioHTML
+        ).join("")
+        : `
+                        <div class="empty-small">
+                            Nenhum áudio registrado.
+                        </div>
+                    `;
+
+
+    document.getElementById(
+        "audioList"
+    ).innerHTML =
+        audios
+            .slice(0, 3)
+            .map(criarAudioHTML)
+            .join("");
+
+    document.getElementById(
+        "allAudios"
+    ).innerHTML = html;
+
+
+    lucide.createIcons();
+}
+
+
+function criarAudioHTML(audio) {
+    return `
+        <div class="audio">
+            <button class="play"
+                    onclick="alert('Aqui será conectado o player de áudio)"
+            >
+
+                <i data-lucide="play"></i>
+            </button>
+
+            <div class="audio=info"
+                <strong>
+                    ${audio.nome}
+                </strong>
+
+                <small>
+                    ${audio.data}
+                </small>
+            </div>
+
+            <small>
+                ${audio.duracao}
+            </small>
+        </div>
+        
+    `;
+}
+
+// ========================================================
+// UPLOAD DE ÁUDIO
+// ========================================================
+
+document.getElementById(
+    "audioUpload"
+).addEventListener(
+    "change",
+    function () {
+        const arquivo = this.files[0];
+
+        if (!arquivo)
+            return;
+
+        alunoSelecionado.audio.push({
+            nome: arquivo.name,
+            data: new Date()
+                .toLocaleDateString(
+                    "pt-BR"
+                ),
+
+            duracao: "Novo áudio"
+        });
+
+        salvarDados();
+
+        atualizarDashboard();
+
+        renderizarAudios();
+
+        this.value = "";
+    }
+);
+
+
+// ========================================================
+// RESPONSÁVEIS
+// ========================================================
+
+function renderizarResponsaveis() {
+
+    const container =
+        document.getElementById(
+            "guardians"
+        );
+
+
+    container.innerHTML =
+        alunoSelecionado.responsaveis
+            .map(
+                responsavel => `
+
+                    <div class="guardian">
+
+                        <div class="guardian-icon">
+
+                            <i data-lucide="user"></i>
+
+                        </div>
+
+                        <div>
+
+                            <strong>
+                                ${responsavel.nome}
+                            </strong>
+
+                            <span>
+                                ${responsavel.parentesco}
+                                •
+                                ${responsavel.telefone}
+                            </span>
+
+                        </div>
+
+                    </div>
+
+                `
+            )
+            .join("");
+
+
+    lucide.createIcons();
+
+}
+
+// ========================================================
+// ABAS
+// ========================================================
+
+function mostrarAba(aba, botao = null) {
+    const abas = [
+        "overview",
+        "ocorrencias",
+        "documentos",
+        "audios",
+        "historico"
+    ];
+
+    abas.forEach(
+        nome => {
+            const elemento = document.getElementById(
+                nome
+            );
+
+            if (elemento) {
+                elemento.style.display = nome === aba
+                    ? "block"
+                    : "none"
+            }
+        }
+    );
+
+    document.querySelectorAll(".tab")
+        .forEach(
+            tab => tab.classList.remove("active")
+        );
+
+    if (botao) {
+        botao.classList.add(
+            "active"
+        );
+    }
+
+    lucide.createIcons();
+}
+
+
+// ========================================================
+// CADASTRO DE ALUNO
+// ========================================================
+
+
+
+
+
+function mostrarTodosAlunos(){
+    document.getElementById(
+        "studentSearch"
+    ).value = "";
+
+    renderizarAlunos();
+}
